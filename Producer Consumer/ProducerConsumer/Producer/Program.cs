b5a285a686e0dbd8e7158ccf74598e311e23af93
Adrian <amacia14@msudenver.edu>
2019-04-23 20:03:36 -0600

    Changes to producer class

--- Producer Consumer/ProducerConsumer/Producer/Program.cs
+++ Producer Consumer/ProducerConsumer/Producer/Program.cs
@@ -1,20 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 
 namespace Producer
 {
-	class Program
+	class Producer
 	{
-		static void Main(string[] args)
-		{
-		}
+		private ConcurrentBuffer _buffer;
 
-		public static void producer()
+		public Producer(ConcurrentBuffer buffer)
 		{
+			_buffer = buffer;
+		}
+		
+		public void producer(int wordCount, int sleepCount)
+		{
+			while(true)
+			{
+				string ayy = Guid.NewGuid().ToString();
+				using(FileStream fileStream = new FileStream("~/Files/" + ayy + ".txt", FileMode.Create))
+				{
+					writeFile(fileStream, wordCount);
+
+					var success = _buffer.TryAdd(ayy);
+					
+					if(success)
+						continue;
+					else
+					{
+						Thread.Sleep(sleepCount * 2);
+					}
+
+				}
+				Thread.Sleep(sleepCount);
+			} 
 			
+		}
+		/// <summary>
+		/// this methods will write set up the writestram to  guid.txt file
+		/// </summary>
+		/// <param name="Filename"></param>
+		static void writeFile(Stream stream, int wordCount)
+		{
+			StreamWriter writer = new StreamWriter(stream);
+
+			for (int wordNumber = 0; wordCount < wordCount; wordNumber++)
+			{ 
+				string input = "LOVE YOU TOO";
+				//Algorithm for random word
+
+				writer.Write(input);
+			}
+			
+			stream.Flush();
 		}
 	}
 }
